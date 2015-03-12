052fdb08f7ff387eeebbfb8091af5402b20f14ee
Kevron Rees <tripzero.kev@gmail.com>
2015-03-11 23:14:52 -0700

    fixed tostring()

--- lights.py
+++ lights.py
@@ -7,13 +7,13 @@ class Ws2801:
 	ledsData = None
 	spiDev = None
 
-	def __init__(self, numLeds):
-		self.ledsData = np.zeros((numLeds,3), np.uint8)
+	def __init__(self, width, height):
+		self.ledsData = np.zeros((width, height, 3), np.uint8)
 		client = iodclient.IodClient()
 		spiDev = client.spi[0]
-		spiDev.write(self.ledsData.toBytes())
+		spiDev.write(self.ledsData.tostring())
 
 	def changeColor(self, ledNumber, color):
 		self.ledsData[ledNumber] = color
-		spiDev.write(self.ledsData.toBytes())
+		spiDev.write(self.ledsData.tostring())
 
