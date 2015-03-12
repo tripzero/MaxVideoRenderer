b0aa743783991abcf63d2a2a3234f1bea288eae8
Kevron Rees <kevron.m.rees@intel.com>
2015-03-12 14:00:07 -0700

    mostly working lights

--- lights.py
+++ lights.py
@@ -2,18 +2,44 @@
 
 import iodclient
 import numpy as np
+import dbus
+from gi.repository import GLib
 
 class Ws2801:
 	ledsData = None
 	spiDev = None
 
 	def __init__(self, ledArraySize):
-		self.ledsData = np.zeros((ledArraySize, 3), np.uint8)
+		self.ledsData = np.zeros((ledArraySize+1, 3), np.uint8)
 		client = iodclient.IodClient()
-		spiDev = client.spi[0]
-		spiDev.write(self.ledsData.tostring())
+		self.spiDev = client.spi[0]
+		self.clear()
+
+	def clear(self):
+		self.ledsData[:] = (0,0,0)
+		self.spiDev.write(dbus.ByteArray(self.ledsData.tostring()))
 
 	def changeColor(self, ledNumber, color):
 		self.ledsData[ledNumber] = color
-		spiDev.write(bytearray(self.ledsData.tostring()))
+		self.spiDev.write(dbus.ByteArray(self.ledsData.tostring()))
+
+	def updateImg(self):
+		self.spiDev.write(dbus.ByteArray(self.ledsData.tostring()))
+
+	def fade(self, img, fade, time):
+		# fade the array 'array', from current level to 'fade' in 'time' ms
+		rate = 0.5 / time
+		step = 0
+		GLib.timeout_add(1, self.fade_step, img, rate, step, time)
+
+	def fade_step(self, img, rate, step, time):
+		img -= img * (1 + rate)
+		self.updateImg()
+
+		step += 1
+		if step >= time:
+			return False
+		return True
+
+
 
--- videocolor.py
+++ videocolor.py
@@ -8,15 +8,14 @@ import argparse
 import videoplayer
 import cv2
 import numpy as np
+import lights
 
 from PyQt5.QtWidgets import QApplication
 
+leds = lights.Ws2801(32)
+
 def colorChanged(r, g, b, id):
-	#img = np.zeros((300, 300, 3), np.uint8)
-	#print "id", id, "color: ", int(r), int(g), int(b)
-	#cv2.rectangle(img, (0,0), (300,300), (r, g, b), -1)
-	#cv2.imshow(str(id), img)
-	pass
+	leds.changeColor(id, (r, g, b))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(prog="maxrenderer", description='minnowboard max dlna renderer with opencv', add_help=False)
