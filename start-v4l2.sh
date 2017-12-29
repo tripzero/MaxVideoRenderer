e8556fbd10b39abe4be350cb8ea8b09f0511f6a2
Kevron Rees <tripzero.kev@gmail.com>
2017-12-29 14:52:26 -0800

    update examples start and service files

--- start-v4l2.sh
+++ start-v4l2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+cd /home/root/src/MaxVideoRenderer
+
+export XDG_RUNTIME_DIR=/run/user/0
+
+pulseaudio --start
+
 pactl load-module module-loopback source=2 sink=0
 
-python3 v4l2player.py
-
+python3 v4l2player.py --test
